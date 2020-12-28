ac5def57946d2ba464eae72b9ab7b7441a9cbf87
JVCubed <46349727+JVCubed@users.noreply.github.com>
2020-12-28 18:50:21 +0100

    Update LogToMySQL.py

--- LogToMySQL.py
+++ LogToMySQL.py
@@ -1,2 +1,26 @@
-# https://www.w3schools.com/python/python_mysql_insert.asp
+#!/usr/bin/python
+
+# imports
 import tailer
+import mysql.connector
+# https://www.w3schools.com/python/python_mysql_insert.asp
+
+# set database information
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+# Follow the file as it grows
+for line in tailer.follow(open('logfile.txt')):
+    # check against blocklist
+    for unwantedUrl in blocklist:
+        # if there exist a line with a entry on the blocklist, do 'telegram-send'
+        if unwantedUrl in line:
+            # disabling potential links to be clickable by putting the dot between brackets
+            safeUrl = line.replace(".", "[.]")
+            telegram_send.send(messages=[safeUrl], conf=None, disable_web_page_preview="true")
