2a192f307f19d421f07e72d5e5ffbbe886fe3b32
JVCubed <j.vdvelden99@outlook.com>
2021-01-02 20:24:06 +0100

    clean up the repo

--- logSSH.py
+++ logSSH.py
@@ -15,24 +15,15 @@ from datetime import datetime
 
 # set database information
 #  https://www.w3schools.com/python/python_mysql_insert.asp
-#  the database and tables have to be created before running this script. below the 'CREATE TABLE' statement I used in this case:
-
-#CREATE TABLE ssh_attempt.log (
-#  id INT AUTO_INCREMENT,
-#  time DATETIME NOT NULL,
-#  username VARCHAR(100),
-#  srcIp VARCHAR(15),
-#  country VARCHAR(2),
-#  PRIMARY KEY(id)
-#);
+#  the database and tables have to be created before running this script. you can use createDatabase.sql for this
 
 #  make sure your user has the correct permissions on the database.
 #  set database information
 mydb = mysql.connector.connect(
   host="localhost",
-  user="python",
-  password="q3#ZJ2cnSP;9W$-[TAFH+mVrfK6%d/_TSwV4pXW@%278^b5xEq}8M~#W$n.C4/U9!JAd;?rGLPqtE(tbJSd$u%-Gmmq*zvvcoa{-",
-  database="ssh_attempt"
+  user="username",
+  password="password",
+  database="database"
 )
 mycursor = mydb.cursor()
 
@@ -40,12 +31,12 @@ mycursor = mydb.cursor()
 reader = geolite2.reader()
 
 # Define strings to check
-userUnknown = ": Failed password for invalid user"
-userKnown = ": Failed password for"
+userFailedUnknown = ": Failed password for invalid user"
+userFailedKnown = ": Failed password for"
 
 # Function to add info to database
 def SQLadd(datetime, user, ip, country):
-  sql = "INSERT INTO log (time, username, srcIp, country) VALUES (STR_TO_DATE(%(date)s,'%Y %b %d %k:%i:%s'), %(name)s, %(ipAddress)s, %(country)s)"
+  sql = "INSERT INTO ssh (time, username, srcIp, country) VALUES (STR_TO_DATE(%(date)s,'%Y %b %d %k:%i:%s'), %(name)s, %(ipAddress)s, %(country)s)"
   mycursor.execute(sql, { 'date': datetime, 'name': user, 'ipAddress': ip, 'country': country})
 
   mydb.commit()
@@ -53,8 +44,8 @@ def SQLadd(datetime, user, ip, country):
 
 # Follow the file as it grows
 for line in tailer.follow(open('/var/log/auth.log')):
-  #
-  if userUnknown in line:
+  
+  if userFailedUnknown in line:
     list = line.split()
     currentYear = str(datetime.now().year)
     date = currentYear + " " + " ".join(list[0:3])
@@ -66,7 +57,7 @@ for line in tailer.follow(open('/var/log/auth.log')):
 
     SQLadd(date, user, ip, country)
 
-  elif userKnown in line:
+  elif userFailedKnown in line:
     list = line.split()
 
     currentYear = str(datetime.now().year)
