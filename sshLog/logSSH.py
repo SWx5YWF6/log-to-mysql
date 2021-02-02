bfb810cfda5c7990f278d32cbd9a7f629ff44010
JVCubed <46349727+JVCubed@users.noreply.github.com>
2021-02-02 15:04:32 +0100

    Updated script

--- sshLog/logSSH.py
+++ sshLog/logSSH.py
@@ -1,71 +1,57 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 # imports
-#  https://pypi.org/project/tailer/
 import tailer
-#  https://pypi.org/project/mysql-connector-python/
+# https://pypi.org/project/tailer/
 import mysql.connector
-# Install geolite2 with
-#  pip3 install maxminddb 
-#  pip3 install maxminddb-geolite2
-#   according to: https://stackoverflow.com/questions/32575666/python-geoip-does-not-work-on-python3-4
-from geolite2 import geolite2
+# https://pypi.org/project/mysql-connector-python/
+# https://www.w3schools.com/python/python_mysql_insert.asp
 from datetime import datetime
+from geolite2 import geolite2
 
+# Variables
+dstCountry = ""
+DBuser = ""
+DBpass = ""
 
 # set database information
-#  https://www.w3schools.com/python/python_mysql_insert.asp
-#  the database and tables have to be created before running this script. you can use createDatabase.sql for this
-
-#  make sure your user has the correct permissions on the database.
-#  set database information
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="username",
-  password="password",
-  database="database"
+  host="88.99.173.15",
+  port="3306",
+  user=DBuser,
+  password=DBpass,
+  database="log"
 )
+
 mycursor = mydb.cursor()
 
-# set reader for GeoIP
 reader = geolite2.reader()
 
-# Define strings to check
-userFailedUnknown = ": Failed password for invalid user"
-userFailedKnown = ": Failed password for"
+user_unknown = ": Failed password for invalid user "
+user_known = ": Failed password for "
 
-# Function to add info to database
 def SQLadd(datetime, user, ip, country):
-  sql = "INSERT INTO ssh (time, username, srcIp, country) VALUES (STR_TO_DATE(%(date)s,'%Y %b %d %k:%i:%s'), %(name)s, %(ipAddress)s, %(country)s)"
-  mycursor.execute(sql, { 'date': datetime, 'name': user, 'ipAddress': ip, 'country': country})
+  sql = "INSERT INTO ssh (time, username, srcIp, srcCountry, dstServer, dstCountry) VALUES (STR_TO_DATE(%(date)s,'%Y %b %d %k:%i:%s'), %(name)s, %(srcIpAddress)s, %(srcCountry)s, %(dstServer)s, %(dstCountry)s)"
+  mycursor.execute(sql, { 'date': datetime, 'name': user, 'srcIpAddress': ip, 'srcCountry': country, 'dstServer': DBuser, 'dstCountry': dstCountry})
 
   mydb.commit()
-  print(datetime, user, ip, country)
+  #print(datetime, user, ip, country)
 
-# Follow the file as it grows
 for line in tailer.follow(open('/var/log/auth.log')):
-  
-  if userFailedUnknown in line:
-    list = line.split()
-    currentYear = str(datetime.now().year)
-    date = currentYear + " " + " ".join(list[0:3])
-    user = list[10]
-
-    ip = list[12]
-    match = reader.get(ip)
-    country = match['country']['iso_code']
-
-    SQLadd(date, user, ip, country)
-
-  elif userFailedKnown in line:
-    list = line.split()
-
-    currentYear = str(datetime.now().year)
-    date = currentYear + " " + " ".join(list[0:3])
-    user = list[8]
-
-    ip = list[10]
-    match = reader.get(ip)
-    country = match['country']['iso_code']
-
-    SQLadd(date, user, ip, country)
+    try:
+        if user_unknown in line or user_known in line:
+            list = line.split()
+            date = str(datetime.now().year) + " " + " ".join(list[:3])
+            var_user = user_unknown if user_unknown in line else user_known
+            user = line.split(var_user,1)[1].rsplit(" from ")[0]
+            ip =list[-4]
+            match = reader.get(ip)
+            try:
+                country = match['country']['iso_code']
+            except:
+                country = ""
+                pass
+            SQLadd(date, user, ip, country)
+    except ValueError as e:
+        print(f'Error: {e}')
+        continue
