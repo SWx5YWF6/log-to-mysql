01fb6471c7aecba64a7e271c66daf50529baa0d3
Zenith <73667419+SWx5YWF6@users.noreply.github.com>
2021-02-03 20:51:49 +0100

    [+] Adjusted schema
    Adjusted DB table schema to support storage of IP version identifier

--- sshLog/createDatabase.sql
+++ sshLog/createDatabase.sql
@@ -3,6 +3,7 @@ CREATE TABLE log.ssh (
           time DATETIME NOT NULL,
           username VARCHAR(100),
           srcIp VARCHAR(45),
+          srcIpType VARCHAR(4), 
           srcCountry VARCHAR(2),
           dstServer VARCHAR(4),
           dstCountry VARCHAR(2),
